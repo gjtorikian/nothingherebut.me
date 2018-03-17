c67855cbaaad26da0d1e1b7cfdad8676b71dc120
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-03-17 17:30:28 -0400

    Trash styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,10 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var sass = require('gulp-ruby-sass');
 var livereload = require('gulp-livereload');
-var gap = require('gulp-append-prepend');
-var cleanCSS = require('gulp-clean-css');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 
 var IS_PRODUCTION = process.env.NODE_ENV == "production";
-
-gulp.task('styles', function() {
-  return sass('assets/styles/*.scss', { style: 'expanded' })
-    .pipe(gap.prependFile('assets/styles/milligram.css'))
-    .pipe(gap.prependFile('assets/styles/normalize.css'))
-    .pipe(gulpif(IS_PRODUCTION, cleanCSS()))
-    .pipe(gulp.dest('public/assets/css'))
-    .pipe(livereload());
-});
 
 gulp.task('scripts', function() {
   return gulp.src('assets/scripts/*.js')
@@ -27,7 +15,6 @@ gulp.task('scripts', function() {
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('assets/styles/*', ['styles']);
   gulp.watch('assets/scripts/*.js', ['scripts']);
   gulp.watch('text.yml', ['server']);
   gulp.watch('views/*.html', ['server']);
@@ -37,5 +24,5 @@ gulp.task('server',function(){
   nodemon();
 });
 
-gulp.task('compile', ['styles', 'scripts']);
+gulp.task('compile', ['scripts']);
 gulp.task('serve', ['compile', 'server', 'watch']);
