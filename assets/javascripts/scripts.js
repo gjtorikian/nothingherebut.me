3deb437e887edd24fa8914845f8d7820a396acd2
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-03-21 10:46:30 -0400

    Format it for moment.js

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -3,20 +3,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var ONE_SECOND = 1000;
 
   function properIsoString(date) {
-      var tzo = -date.getTimezoneOffset(),
-          dif = tzo >= 0 ? '+' : '-',
-          pad = function(num) {
+      var pad = function(num) {
               var norm = Math.floor(Math.abs(num));
               return (norm < 10 ? '0' : '') + norm;
           };
       return date.getFullYear() +
           '-' + pad(date.getMonth() + 1) +
           '-' + pad(date.getDate()) +
-          'T' + pad(date.getHours()) +
+          ' ' + pad(date.getHours()) +
           ':' + pad(date.getMinutes()) +
-          ':' + pad(date.getSeconds()) +
-          dif + pad(tzo / 60) +
-          ':' + pad(tzo % 60);
+          ':' + pad(date.getSeconds())
   }
 
   // compatible with IE7+, Firefox, Chrome, Opera, Safari
